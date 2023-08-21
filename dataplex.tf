fdfa1a92be7e3ef1203928d15c1d5f91e5414f3c
test1 <black.comp76@gmail.com>
2023-08-20 21:06:02 -0400

    infra base

--- dataplex.tf
+++ dataplex.tf
@@ -1,7 +1,7 @@
 module "dataplex_lake1" {
   source = "./modules/env_dataplex"
 
-  name     = "jenkins_init"
+  name     = "ki"
   location     = "us-central1"
   project = "possible-byway-392901"
 }
